35c46a43a8377bfae5b042b0e2373b4e874d84d9
Tim Huijg <tim_huijg@hotmail.com>
2021-06-06 12:35:26 +0200

    Initial commit

---
+++ src/components/image/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './index.module.scss'
+import NextImage from 'next/image'
+
+export default function Image ({
+  image
+}) {
+
+  const imageService = ' https://img2.storyblok.com/'
+  const newPath = image.src?.replace('https://a.storyblok.com/', '')
+  const transformedSrc = imageService + (image.focusPoint ? `filters:focal(${image.focusPoint})/` : '') + newPath
+
+  const myLoader = ({ width }) => {
+    return imageService + `${width}x0/` + (image.focusPoint ? `filters:focal(${image.focusPoint})/` : '') + newPath
+  }
+
+  return (
+    <div className={styles.container} style={{ paddingTop: `min(calc(100% * ${image.aspectRatio}))`}}>
+      <NextImage
+        loader={myLoader}
+        src={transformedSrc}
+        alt={image.alt}
+        layout="fill"
+      />
+    </div>
+  )
+}
